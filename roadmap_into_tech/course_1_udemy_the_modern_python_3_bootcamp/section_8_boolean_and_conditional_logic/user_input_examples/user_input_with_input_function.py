7db70ce2bd54e9904e086fbec8fa92964aaa0575
= <=>
2022-09-29 18:17:39 -0400

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/user_input_examples/user_input_with_input_function.py
@@ -0,0 +1,7 @@
+'''
+Below is an example of how you can get user input by using one line with the
+input() function
+'''
+
+name = input("Enter your name here: ")
+print("You said your name is " + name)
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/user_input_examples/user_input_with_print_function.py
@@ -0,0 +1,8 @@
+'''
+Below is an example of how you can get user input by using a seperate line with
+the print() function
+'''
+
+print("What's your favorite color?")
+data = input()
+print("You said " + data)
