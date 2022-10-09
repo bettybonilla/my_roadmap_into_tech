f939a9576adae0c0edde7dd2a21e938cc534af22
= <=>
2022-10-09 17:52:38 -0400

    Refactor: Fix comments for accuracy

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/user_input/user_input_with_input_function.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/user_input/user_input_with_input_function.py
@@ -1,15 +1,16 @@
 '''
-The below shows 2 ways you can get user input with the input() function
+The below shows 2 ways you can prompt for user input with the print() function
+or input() function
 '''
 
-# This is how you can get user input by using a seperate line with
+# This is how you can prompt a user to input data by using a seperate line with
 # the print() function
 print("What's your favorite color?")
 data = input()
 print("You said " + data)
 
-# This is another way you can get user input by using one line with
-# the input() function as opposed to using a seperate line with
+# This is another way you can prompt a user to input data by using one line
+# with the input() function as opposed to using a seperate line with
 # the print() function
 data = input("What's your favorite color? ")
 print("You said " + data)
