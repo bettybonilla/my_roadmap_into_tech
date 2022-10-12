aff40a08a429ef0521c38fab7a85d3f579ff665b
= <=>
2022-10-12 18:07:47 -0400

    Add course file

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_statements/multiple_elifs.py
@@ -0,0 +1,16 @@
+'''
+Below is an example of how you can use multiple elif conditionals in a
+conditional statement
+'''
+
+color = input("What's your favorite color? ").lower().strip()
+if color == "purple":
+    print("excellent choice!")
+elif color == "teal":
+    print("not bad!")
+elif color == "seafoam":
+    print("mediocre")
+elif color == "pure darkness":
+    print("I like how you think")
+else:
+    print("YOU MONSTER!")
