718c25bb9ef06035d8688e1920057f4eeab8430f
= <=>
2022-10-19 17:58:35 -0400

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/exercise_10_classifying_food.py
@@ -0,0 +1,26 @@
+'''
+- I've written some code at the top of the file for you. Please don't touch it,
+if you'd like the tests to work :)
+- All the code does is randomly set the food variable to either “apple”,
+“grape”, “bacon”, “steak”, “worm”, or “dirt”
+- When you run the prewritten code, the food variable will be randomly
+assigned. Your task is to write code that will classify the food variable.
+- If food is set to either “apple” or “grape”, your code should print “fruit”
+- If food is set to either “bacon” or “steak”, your code should print “meat”
+- If food is set to either “dirt” or “worm” your code should print “yuck”
+'''
+
+# NO TOUCHING ======================================================
+from random import choice
+food = choice(['apple', 'grape', 'bacon', 'steak', 'worm', 'dirt'])
+# NO TOUCHING ======================================================
+
+# YOUR CODE GOES HERE:
+print(food)
+
+if food == "apple" or food == "grape":
+    print("fruit")
+if food == "bacon" or food == "steak":
+    print("meat")
+if food == "dirt" or food == "worm":
+    print("yuck")
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/not_logical_operator.py
@@ -0,0 +1,16 @@
+'''
+The below is a conditional statement using the not logical operator
+- Movie ticket price for ages 2 to 8 years old: 2 dollars
+- Movie ticket price for ages 65 years old and older: 5 dollars
+- Movie ticket price for everyone else: 10 dollars
+'''
+
+age = input("Enter your age: ")
+age = float(age)
+
+# Anything inside parentheses () will be the opposite value when you use
+# the not logical operator in front of it
+if not ((age >= 2 and age <= 8) or age >= 65):
+    print("YOU ARE NOT A CHILD OR SENIOR SO YOU PAY 10 DOLLARS!")
+else:
+    print("YOU ARE A CHILD OR SENIOR!")
