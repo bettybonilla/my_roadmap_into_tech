241ea3d7bb1f55bb070bfac43f30eb0248c3fdd7
= <=>
2022-10-29 19:04:58 -0400

    Style: Update formatting for consistency

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/exercise_9_number_is_odd.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/exercise_9_number_is_odd.py
@@ -1,15 +1,15 @@
 '''
 - You will be provided with a random number in a variable called num
-- Use a conditional statement to check if the number is odd. If num is odd,
-print "odd". Otherwise print "even".
+- Use a conditional statement to check if the number is odd
+- If num is odd, print "odd". Otherwise, print "even".
 - Hint: Use modulus % to figure out if the number is odd!
 '''
 
-# NO TOUCHING ======================================
+# NO TOUCHING =================================================================
 # Picks random number from 1-1000
 from random import randint
 num = randint(1, 1000)
-# NO TOUCHING ======================================
+# NO TOUCHING =================================================================
 
 # YOUR CODE GOES HERE:
 print(num)
