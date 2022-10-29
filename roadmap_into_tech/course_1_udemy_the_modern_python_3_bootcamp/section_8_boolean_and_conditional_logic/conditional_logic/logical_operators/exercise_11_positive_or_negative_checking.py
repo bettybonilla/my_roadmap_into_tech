46bd35631a7facd408e2cc3bf0fad9bcbbe2c30f
= <=>
2022-10-29 19:05:14 -0400

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/exercise_11_positive_or_negative_checking.py
@@ -0,0 +1,34 @@
+'''
+- In this exercise x and y are two random variables. The code at the top of
+the file randomly assigns them (we'll learn how it works later on) but for now
+just leave it alone :)
+- If both are positive numbers, print "both positive"
+- If both are negative numbers, print "both negative"
+- Otherwise, tell us which one is positive and which one is negative
+- Ex: "x is positive and y is negative"
+- NOTE: The print statements are filled in for you, just add logic - For the
+tests to pass, don't change the print statements!
+'''
+
+# NO TOUCHING =================================================================
+from random import randint
+x = randint(-100, 100)
+while x == 0:  # Makes sure x isn't zero
+    x = randint(-100, 100)
+y = randint(-100, 100)
+while y == 0:  # Makes sure y isn't zero
+    y = randint(-100, 100)
+# NO TOUCHING =================================================================
+
+# YOUR CODE GOES HERE:
+print("x = {}".format(x))
+print("y = {}".format(y))
+
+if x > 0 and y > 0:
+    print("both positive")
+elif x < 0 and y < 0:
+    print("both negative")
+elif x > 0 and y < 0:
+    print("x is positive and y is negative")
+elif x < 0 and y > 0:
+    print("y is positive and x is negative")
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/exercise_12_calling_in_sick.py
@@ -0,0 +1,45 @@
+'''
+- In this exercise you will be given a few variables that will be set randomly
+to boolean values (True or False):
+    - actually_sick - When you legit have the flu!
+    - kinda_sick - You're feeling under the weather and it's enough to treat
+    yourself with a day off (if you can spare it)
+    - hate_your_job - Work sucks, I know...
+- You're also given a random number of sick_days between 0 and 10
+- Finally, there is a variable called calling_in_sick that you must set to
+True or False based on the following scenarios:
+    - Set to True if:
+        - You're actually_sick and you have sick_days remaining
+        - You're kinda_sick and hate_your_job and you have sick_days remaining
+    - Otherwise, set to False if:
+        - The tests check that the value of calling_in_sick is correct based
+        on the conditions specified above
+'''
+
+# NO TOUCHING =================================================================
+# Randomly assigns values to these four variables
+from random import choice, randint
+actually_sick = choice([True, False])
+kinda_sick = choice([True, False])
+hate_your_job = choice([True, False])
+sick_days = randint(0, 10)
+# NO TOUCHING =================================================================
+
+# Set this to True or False with boolean logic and conditionals!
+calling_in_sick = None
+
+# YOUR CODE GOES HERE:
+print("actually_sick =", actually_sick)
+print("kinda_sick =", kinda_sick)
+print("hate_your_job =", hate_your_job)
+print("sick_days =", sick_days)
+
+if actually_sick and sick_days > 0:
+    calling_in_sick = True
+    print("calling_in_sick =", calling_in_sick)
+elif kinda_sick and hate_your_job and sick_days > 0:
+    calling_in_sick = True
+    print("calling_in_sick =", calling_in_sick)
+else:
+    calling_in_sick = False
+    print("calling_in_sick=", calling_in_sick)
