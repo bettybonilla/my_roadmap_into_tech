d460ee6b0d05fe089b18b5a200bd852710d5311e
= <=>
2022-11-24 17:01:03 -0500

    Style: Update formatting for consistency

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/exercise_10_classifying_food.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/exercise_10_classifying_food.py
@@ -7,7 +7,7 @@ if you'd like the tests to work :)
 assigned. Your task is to write code that will classify the food variable.
 - If food is set to either “apple” or “grape”, your code should print “fruit”
 - If food is set to either “bacon” or “steak”, your code should print “meat”
-- If food is set to either “dirt” or “worm” your code should print “yuck”
+- If food is set to either “dirt” or “worm”, your code should print “yuck”
 '''
 
 # NO TOUCHING =================================================================
