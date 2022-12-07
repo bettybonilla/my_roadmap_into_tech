b2e7400118daa2a1c4e22d2d4fe30b12bbb06801
= <=>
2022-12-06 19:20:48 -0500

    Refactor: Update comments

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_6_numbers_operators_and_comments/comments.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_6_numbers_operators_and_comments/comments.py
@@ -1,6 +1,7 @@
 '''
-This is a block comment, done at the very top of a file and it's used to
-describe the entire file's functionality/intent or anything complex
+This is a block comment, usually done at the very top of a file to describe
+the entire file's functionality/intent. It can also be used within your code
+to describe anything complex.
 '''
 
 # This is an inline comment
