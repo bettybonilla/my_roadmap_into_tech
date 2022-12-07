4a3b6a481ad880cf45a1f171ebafb4076c38bb3c
= <=>
2022-12-07 18:07:40 -0500

    Refactor: Add alternative code

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/leetcode.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_8_boolean_and_conditional_logic/conditional_logic/logical_operators/leetcode.py
@@ -11,20 +11,36 @@ Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz",
 "13","14","FizzBuzz"]
 '''
 
-# ----------------------------------------------------------------------------
+# ============================================================================
 # TODO Revisit after learning arrays and for loops
 # The code below is correct but incomplete so it would not be ready for
 # submission
-# ----------------------------------------------------------------------------
+# ============================================================================
+
+
+def process_data(n):
+    n = int(n)
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+
 
 n = input("Enter a number: ")
-n = int(n)
+process_data(n)
+print(str(n), type(str(n)))
 
-if n % 3 == 0 and n % 5 == 0:
-    print("FizzBuzz")
-elif n % 3 == 0:
-    print("Fizz")
-elif n % 5 == 0:
-    print("Buzz")
-else:
-    print(str(n), type(str(n)))
+# The code below is alternative code to the code above
+# n = input("Enter a number: ")
+# n = int(n)
+
+# if n % 3 == 0 and n % 5 == 0:
+#     print("FizzBuzz")
+# elif n % 3 == 0:
+#     print("Fizz")
+# elif n % 5 == 0:
+#     print("Buzz")
+# else:
+#     print(str(n), type(str(n)))
