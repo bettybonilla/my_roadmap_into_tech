57f1425680aca5eeda46ae7643bfa05141bc58a3
= <=>
2023-01-10 18:24:00 -0500

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/for_loop.py
@@ -0,0 +1,15 @@
+"""
+Below are some examples of for loops
+"""
+
+# The code below is an example of a for loop with a range from 1 to 10
+for x in range(1, 10):
+    print(x)
+    # print(x * x)
+
+print("\n")
+
+# The code below is an example of a for loop with the string "coffee"
+for letter in "coffee":
+    print(letter)
+    # print(letter * 5)
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/range_function.py
@@ -0,0 +1,16 @@
+"""
+Below are some examples of for loops with different range() functions
+"""
+
+for num in range(10):
+    print(num)
+
+print("\n")
+
+for num in range(10, 20, 2):
+    print(num)
+
+print("\n")
+
+for num in range(0, 100, 5):
+    print(num)
