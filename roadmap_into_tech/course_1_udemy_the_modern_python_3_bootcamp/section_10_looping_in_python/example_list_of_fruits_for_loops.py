136060d24db0a543cdf0a3ccaa808cdcd2f8278b
= <=>
2023-01-11 17:07:27 -0500

    Add example file

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/example_list_of_fruits_for_loops.py
@@ -0,0 +1,36 @@
+# ============================================================================
+# TODO Revisit and test knowledge
+# ============================================================================
+
+# my_list_of_fruits = ["apples", "pears", "grapes", "avocados"]
+
+# # creates copies (local variables)
+# for fruit in my_list_of_fruits:
+#     print("copy of " + fruit)
+
+# # access fruits directly
+# for i in range(len(my_list_of_fruits)):
+#     print("fruit item " + my_list_of_fruits[i])
+
+
+my_list_of_fruits = ["apples", "pears", "grapes", "avocados"]
+
+# creates copies (local variables)
+for fruit in my_list_of_fruits:
+    fruit = fruit + "modified"
+    print("copy of " + fruit)
+
+# creates copies (local variables)
+for fruit in my_list_of_fruits:
+    print("copy of 2 " + fruit)
+
+# access fruits directly
+for i in range(len(my_list_of_fruits)):
+    my_list_of_fruits[i] = my_list_of_fruits[i] + " modified"
+    print("fruit item " + my_list_of_fruits[i])
+
+# access fruits directly
+for i in range(len(my_list_of_fruits)):
+    print("fruit item 2 " + my_list_of_fruits[i])
+
+# https://stackoverflow.com/questions/988155/how-do-i-operate-on-the-actual-object-not-a-copy-in-a-python-for-loop
