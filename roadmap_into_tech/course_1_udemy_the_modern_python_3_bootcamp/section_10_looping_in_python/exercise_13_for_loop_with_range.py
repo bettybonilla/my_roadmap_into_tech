ca337fd446f65ee41508097cb3cf12eb99ad9aff
= <=>
2023-01-11 17:06:40 -0500

    Add course file

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/exercise_13_for_loop_with_range.py
@@ -0,0 +1,14 @@
+"""
+Use a for loop to add up every odd number from 10 to 20 (inclusive) and store
+the result in the variable x
+"""
+
+# Add up all odd numbers between 10 and 20
+# Store the result in x:
+x = 0
+
+# YOUR CODE GOES HERE:
+for i in range(11, 21, 2):
+    # print(i)
+    x += i
+print(x)
