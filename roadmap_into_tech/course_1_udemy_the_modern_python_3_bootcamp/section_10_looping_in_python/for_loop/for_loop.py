347b423ec632c90c359b4b3ccd8c672b410eb040
= <=>
2023-03-03 16:38:32 -0500

    Refactor: Update comments and code
    Replaced print statements to add 1 new line instead of 2 new lines

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/for_loop/for_loop.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/for_loop/for_loop.py
@@ -7,7 +7,12 @@ for x in range(1, 10):
     print(x)
     # print(x * x)
 
-print("\n")
+# When using the print() function, print("") is the default which is why your
+# output always appears on a new line - Using print(end="\n") is the same as
+# print("")
+# This is why print("\n") adds 2 new lines
+# print("\n")
+print("")
 
 # The code below is an example of a for loop with the string "coffee"
 for letter in "coffee":
