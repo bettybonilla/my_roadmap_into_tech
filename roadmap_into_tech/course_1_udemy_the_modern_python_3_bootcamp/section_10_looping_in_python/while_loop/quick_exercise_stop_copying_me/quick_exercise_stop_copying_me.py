de21fe6af21e7b31326e6e0ed3cbeba4c10c53b1
= <=>
2023-04-19 19:56:38 -0400

    Refactor: Remove methods

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/while_loop/quick_exercise_stop_copying_me/quick_exercise_stop_copying_me.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_10_looping_in_python/while_loop/quick_exercise_stop_copying_me/quick_exercise_stop_copying_me.py
@@ -3,10 +3,10 @@ Repeat everything until the user says "stop copying me"
 """
 
 sister = print("Hey, how's it going?")
-brother = input().lower()
+brother = input()
 
 while brother != "stop copying me":
     sister = brother
     print(sister)
-    brother = input().lower()
+    brother = input()
 print("UGH FINE YOU WIN 🙄")
