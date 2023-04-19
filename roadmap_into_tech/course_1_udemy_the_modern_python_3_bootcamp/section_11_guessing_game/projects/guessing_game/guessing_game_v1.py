f57e4e44cb4bf655eecd45e759f4ea3c7fd752e7
= <=>
2023-04-19 19:47:24 -0400

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_11_guessing_game/projects/guessing_game/guessing_game_v1.py
@@ -0,0 +1,63 @@
+"""
+- The program below is a guessing game for PvC (Player vs. Computer) where the
+player has to guess the random number the computer picks between 1 and 10 and
+the computer gives hints to help the player guess the number by stating if the
+number guessed was too high or too low
+- The player is also asked if they would like to play again
+"""
+
+import random
+import time
+
+time.sleep(0.5)
+print("Welcome!\n")
+
+time.sleep(0.5)
+print("Guess...")
+time.sleep(0.5)
+print("the...")
+time.sleep(0.5)
+print("number!\n")
+time.sleep(0.5)
+
+# Generates a random number between 1 and 10 (inclusive)
+computer = random.randint(1, 10)
+
+while True:
+    # Variable shadowing should be avoided which is when you reuse the same
+    # exact variable name for altered data
+    # When you validate data, it's good practice to rename variables
+    # separately with the altered data since the data has been altered from
+    # the original variable and this way you can still access the original
+    # variable with the unaltered data if needed
+    # However, if you don't need to have 2 separate variables, you can also
+    # cast the input() function inside the int() type conversion function
+    # player_input = int(input("Guess the number between 1 and 10: ").strip())
+    player_input_str = input("Guess the number between 1 and 10: ").strip()
+    player_input_int = int(player_input_str)
+
+    if player_input_int == computer:
+        print("You guessed it! You win!")
+        time.sleep(0.5)
+        play_again = input("Do you want to play again? (y/n): ").strip().lower()
+        # Below is another way to look for user input by using the
+        # .find() method for variables with string data types
+        # The > -1 refers to looking for the string index of "y" in the user
+        # input and if the string index of "y" is greater than -1, then
+        # this means "y" would be found indexed 0 or greater in the user input
+        # and would make the if conditional truthy which would execute the next
+        # lines of code
+        # The .strip() method above wouldn't be needed since the
+        # .find() method will find the string regardless of whitespaces
+        # if play_again.find("y") > -1:
+        if play_again[0] == "y":
+            time.sleep(0.5)
+            computer = random.randint(1, 10)
+        elif play_again[0] == "n":
+            break
+    elif player_input_int > computer:
+        print("Too high, try again!")
+    elif player_input_int < computer:
+        print("Too low, try again!")
+time.sleep(0.5)
+print("Thanks for playing, Bye!")
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_11_guessing_game/projects/guessing_game/guessing_game_v1_with_ctrl_c.py
@@ -0,0 +1,67 @@
+"""
+- The program below is a guessing game for PvC (Player vs. Computer) where the
+player has to guess the random number the computer picks between 1 and 10 and
+the computer gives hints to help the player guess the number by stating if the
+number guessed was too high or too low
+- The player is also given the option to exit the game by pressing Ctrl + C
+"""
+
+import random
+import signal
+import time
+
+
+# A Signal Handler is a user defined function, as shown below, where Python
+# signals can be handled
+# Below, the signal_handler() function gracefully exits the program and
+# provides an exit message to the user
+def signal_handler(signum, frame):
+    time.sleep(0.5)
+    print("")
+    print("Thanks for playing, Bye!")
+    # Using exit(0) or quit(0) tells the program to exit/quit successfully
+    # because there were no errors and also ensures you terminate any loops
+    # still running inside the program
+    exit(0)
+
+
+# The signal.SIGINT (SIGINT = Interrupt Signal AKA Ctrl + C) would be the
+# default behavior to stop the current program running
+# However, we can assign our own signal handler to detect this signal and do
+# our custom processing instead
+# Below, (signal.SIGINT, signal_handler) registers our
+# signal_handler() function with signal.SIGINT (Ctrl + C)
+# Therefore, after we run the program and press Ctrl + C, the program will go
+# to our signal_handler() function
+# https://www.askpython.com/python-modules/python-signal#:~:text=A%20Signal%20Handler%20is%20a,do%20our%20custom%20processing%20instead!
+signal.signal(signal.SIGINT, signal_handler)
+
+time.sleep(0.5)
+print("Welcome!\n")
+
+time.sleep(0.5)
+print("Guess...")
+time.sleep(0.5)
+print("the...")
+time.sleep(0.5)
+print("number!\n")
+time.sleep(0.5)
+
+print("To exit the game, press Ctrl + C\n")
+time.sleep(0.5)
+
+# Generates a random number between 1 and 10 (inclusive)
+computer = random.randint(1, 10)
+
+while True:
+    player_input_str = input("Guess the number between 1 and 10: ").strip()
+    player_input_int = int(player_input_str)
+
+    if player_input_int == computer:
+        print("You guessed it! You win!")
+        time.sleep(0.5)
+        computer = random.randint(1, 10)
+    elif player_input_int > computer:
+        print("Too high, try again!")
+    elif player_input_int < computer:
+        print("Too low, try again!")
