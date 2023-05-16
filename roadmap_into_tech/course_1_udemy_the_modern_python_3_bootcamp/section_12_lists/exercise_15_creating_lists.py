c97faaf39d05033dcd041bd4f2545cb7b8264448
= <=>
2023-05-16 15:42:13 -0400

    Add course file

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_12_lists/exercise_15_creating_lists.py
@@ -0,0 +1,20 @@
+"""
+- It's time to create your own lists!
+- Define 2 separate lists:
+    - First, define a list called my_stuff
+        - It must be at least 4 elements long - The data is completely up
+        to you, but it must contain at least 1 string and 1 float
+    - Next, define a list called nums
+        - It should be a list containing all the numbers between 1 and 99
+        (including 99)
+        - Hint: Don't type this out manually! Use what we learned at the end
+        of the previous video!
+"""
+
+# Define my_stuff
+my_stuff = ["backpack", "notebook", "AirPods Pro", 2.0]
+print(my_stuff)
+
+# Define nums
+nums = list(range(1, 100))
+print(nums)
