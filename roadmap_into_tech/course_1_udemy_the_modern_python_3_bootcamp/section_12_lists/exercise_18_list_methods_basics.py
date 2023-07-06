9eca796fb38e0929d6de796a5f1da9c95052b6b3
= <=>
2023-07-06 17:54:20 -0400

    Add course file

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_12_lists/exercise_18_list_methods_basics.py
@@ -0,0 +1,16 @@
+"""
+Now that we've learned about lists, let's get some practice! See the problem
+statements in the comments below
+"""
+
+# Initialize an empty list called instructors
+instructors = []
+
+# Add the following strings to the instructors list
+# "Colt"
+# "Blue"
+# "Lisa"
+instructors.extend(["Colt", "Blue", "Lisa"])
+
+# Run the tests to make sure you've done this correctly!
+print(instructors)
