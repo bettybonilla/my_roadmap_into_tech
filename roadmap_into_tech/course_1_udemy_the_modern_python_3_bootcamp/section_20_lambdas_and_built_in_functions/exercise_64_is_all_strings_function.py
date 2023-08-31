7ff43424bf33d259808b07fc1b3cf9a8080ea784
= <=>
2023-08-31 17:47:36 -0400

    Add course files

---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_20_lambdas_and_built_in_functions/exercise_64_is_all_strings_function.py
@@ -0,0 +1,20 @@
+"""
+Implement a function is_all_strings that accepts a single iterable and returns
+True if it contains ONLY strings - Otherwise, it should return false
+- Ex:
+    is_all_strings(['a', 'b', 'c'])  # True
+    is_all_strings([2, 'a', 'b', 'c'])  # False
+    is_all_strings(('hello', 'goodbye'))  # True
+"""
+
+
+def is_all_strings(iterable: str | list | tuple | dict | set) -> bool:
+    # Using a generator expression instead of a list comprehension since I
+    # don't need to return a list
+    return all((type(i) == str for i in iterable))
+
+
+print(is_all_strings(["a", "b", "c"]))
+print(is_all_strings([2, "a", "b", "c"]))
+print(is_all_strings(("hello", "goodbye")))
+print(is_all_strings("puppies"))
---
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_20_lambdas_and_built_in_functions/list_comprehension_vs_generator_expression.py
@@ -0,0 +1,16 @@
+"""
+The below shows the difference in memory between using a list comprehension vs.
+a generator expression
+"""
+
+import sys
+
+# The sys.getsizeof() method gets the size in bytes of memory being used
+list_comp = sys.getsizeof([x * 10 for x in range(1000)])
+gen_expression = sys.getsizeof((x * 10 for x in range(1000)))
+
+# Using generator expressions are more memory efficient than using list
+# comprehensions
+print("To do the same thing, it took...")
+print(f"List comprehension: {list_comp} bytes")
+print(f"Generator expression: {gen_expression} bytes")
