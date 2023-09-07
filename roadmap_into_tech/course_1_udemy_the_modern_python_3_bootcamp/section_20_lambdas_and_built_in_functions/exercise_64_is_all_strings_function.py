dac5484e2c4e44dd4b077e86c39d187a751a8326
= <=>
2023-09-06 20:07:22 -0400

    Style: Fix typo

--- roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_20_lambdas_and_built_in_functions/exercise_64_is_all_strings_function.py
+++ roadmap_into_tech/course_1_udemy_the_modern_python_3_bootcamp/section_20_lambdas_and_built_in_functions/exercise_64_is_all_strings_function.py
@@ -1,6 +1,6 @@
 """
 Implement a function is_all_strings that accepts a single iterable and returns
-True if it contains ONLY strings - Otherwise, it should return false
+True if it contains ONLY strings - Otherwise, it should return False
 - Ex:
     is_all_strings(['a', 'b', 'c'])  # True
     is_all_strings([2, 'a', 'b', 'c'])  # False
