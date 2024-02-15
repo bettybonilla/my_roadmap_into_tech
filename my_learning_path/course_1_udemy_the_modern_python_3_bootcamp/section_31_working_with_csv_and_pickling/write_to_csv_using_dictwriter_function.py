b9396214a05d5fcd6d3d0a0f1a7f827b2bac2f39
= <=>
2024-02-15 18:00:33 -0500

    Refactor: Rename variable

--- my_learning_path/course_1_udemy_the_modern_python_3_bootcamp/section_31_working_with_csv_and_pickling/write_to_csv_using_dictwriter_function.py
+++ my_learning_path/course_1_udemy_the_modern_python_3_bootcamp/section_31_working_with_csv_and_pickling/write_to_csv_using_dictwriter_function.py
@@ -9,11 +9,11 @@ with open("cats.csv", "w") as file:
     headers = ["Name", "Age"]
     # Then you can create a DictWriter object for writing to CSV files using the DictWriter() function and pass in the
     # CSV file and the fieldnames keyword argument which you assign to your headers
-    csv_writer_file = csv.DictWriter(file, fieldnames=headers)
+    csv_dictwriter_file = csv.DictWriter(file, fieldnames=headers)
     # Then you can use the .writeheader() method on the DictWriter object to write the header row to the CSV file which
     # uses the fieldnames keyword argument (you don't need to pass it in)
-    csv_writer_file.writeheader()
+    csv_dictwriter_file.writeheader()
     # Then finally you can use the .writerow() method on the DictWriter object to write a row to the CSV file by passing
     # in a dictionary
-    csv_writer_file.writerow({"Name": "Blue", "Age": 3})
-    csv_writer_file.writerow({"Name": "Garfield", "Age": 10})
+    csv_dictwriter_file.writerow({"Name": "Blue", "Age": 3})
+    csv_dictwriter_file.writerow({"Name": "Garfield", "Age": 10})
