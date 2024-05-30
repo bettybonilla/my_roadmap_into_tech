04557e57e0ff27df8863cbb4504494d903a75741
= <=>
2024-05-30 19:20:29 -0400

    Style: Update formatting

--- my_learning_path/course_1_udemy_the_modern_python_3_bootcamp/section_36_massive_section_of_challenges/exercises/exercise_109_list_check_function.py
+++ my_learning_path/course_1_udemy_the_modern_python_3_bootcamp/section_36_massive_section_of_challenges/exercises/exercise_109_list_check_function.py
@@ -1,6 +1,6 @@
 """
-- Write a function called list_check which accepts a list and returns True if each value in the list is a list
-    - Otherwise, the function should return False
+Write a function called list_check which accepts a list and returns True if each value in the list is a list
+- Otherwise, the function should return False
 - Ex:
     list_check([[], [1], [2,3], (1,2)])  # False
     list_check([1, True, [], [1], [2,3]])  # False
