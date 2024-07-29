90849768af6a22178745f1bb8655d448a1689658
= <=>
2024-07-28 22:19:35 -0400

    Feat: Add prints for when list is already sorted vs. was sorted

--- my_learning_path/course_3_udemy_computer_science_101_master_the_theory_behind_programming/section_6_sorting_algorithms/insertion_sort.py
+++ my_learning_path/course_3_udemy_computer_science_101_master_the_theory_behind_programming/section_6_sorting_algorithms/insertion_sort.py
@@ -6,6 +6,9 @@ The below represents insertion sort in code
 def insertion_sort(your_list: list[int]):
     n = len(your_list)
 
+    # Keep track of insertion
+    insertion_ready = False
+
     for i in range(1, n):
         key = your_list[i]
         j = i - 1
@@ -16,12 +19,23 @@ def insertion_sort(your_list: list[int]):
             your_list[j + 1] = your_list[j]
             j -= 1
 
+            # Keep track of insertion
+            insertion_ready = True
+
         # Insert key in the correct position
         your_list[j + 1] = key
+
+        # If no insertion occurred this means the list is already sorted therefore we can early escape
+        if not insertion_ready:
+            print("List is already sorted")
+            break
+
+    if insertion_ready:
+        print("List sorted in ascending order")
 
 
 if __name__ == "__main__":
     data = [9, 5, 1, 4, 3]
+    # data = [1, 2, 3, 4, 5]
     insertion_sort(data)
-    print("Sorted list in ascending order")
     print(data)
