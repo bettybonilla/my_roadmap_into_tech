9a0e3a2e2e161a1b0d15963f2ad42d41f0336de7
= <=>
2024-10-07 17:56:23 -0400

    Add course files

---
+++ my_learning_path/course_4_udemy_data_structures_and_algorithms_python/section_9_doubly_linked_lists_leet_code_exercises/exercise_30_swap_first_and_last.py
@@ -0,0 +1,71 @@
+"""
+Swap the values of the first and last node
+- Method name: swap_first_last
+- Note that the pointers to the nodes themselves are not swapped - Only their values are exchanged
+"""
+
+from typing import Any
+
+
+class Node:
+    # Initializer AKA constructor
+    def __init__(self, value: Any):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    # Initializer AKA constructor
+    def __init__(self, value: Any):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value: Any) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def swap_first_last(self):
+        if self.length > 1:
+            first_node_value = self.head.value
+            last_node_value = self.tail.value
+            self.head.value = last_node_value
+            self.tail.value = first_node_value
+
+    def print_list(self):
+        if self.head is None:
+            print("Empty linked list")
+        else:
+            values = []
+            current_node = self.head
+            values.append("None")
+            while current_node:
+                values.append(str(current_node.value))
+                current_node = current_node.next
+            values.append("None")
+            print(" <-> ".join(values))
+
+
+if __name__ == "__main__":
+    my_doubly_linked_list = DoublyLinkedList(1)
+    my_doubly_linked_list.append(2)
+    my_doubly_linked_list.append(3)
+    my_doubly_linked_list.append(4)
+
+    print("DLL before swap_first_last():")
+    my_doubly_linked_list.print_list()
+
+    my_doubly_linked_list.swap_first_last()
+
+    print("\nDLL after swap_first_last():")
+    my_doubly_linked_list.print_list()
---
+++ my_learning_path/course_4_udemy_data_structures_and_algorithms_python/section_9_doubly_linked_lists_leet_code_exercises/exercise_31_reverse.py
@@ -0,0 +1,80 @@
+"""
+Create a new method called reverse that reverses the order of the nodes in the linked list (i.e., the first node becomes
+the last node, the second node becomes the second-to-last node, and so on)
+- To do this, you'll need to traverse the linked list and change the direction of the pointers between the nodes so that
+they point in the opposite direction
+- Do not change the value of the nodes
+- Once you've done this for all nodes, you'll also need to update the head and tail pointers to reflect the new order of
+the nodes
+"""
+
+from typing import Any
+
+
+class Node:
+    # Initializer AKA constructor
+    def __init__(self, value: Any):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    # Initializer AKA constructor
+    def __init__(self, value: Any):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value: Any) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def reverse(self):
+        current_node = self.head
+        while current_node:
+            temp_next = current_node.next
+            current_node.next = current_node.prev
+            current_node.prev = temp_next
+            current_node = temp_next
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+    def print_list(self):
+        if self.head is None:
+            print("Empty linked list")
+        else:
+            values = []
+            current_node = self.head
+            values.append("None")
+            while current_node:
+                values.append(str(current_node.value))
+                current_node = current_node.next
+            values.append("None")
+            print(" <-> ".join(values))
+
+
+if __name__ == "__main__":
+    my_doubly_linked_list = DoublyLinkedList(1)
+    my_doubly_linked_list.append(2)
+    my_doubly_linked_list.append(3)
+    my_doubly_linked_list.append(4)
+    my_doubly_linked_list.append(5)
+
+    print("DLL before reverse():")
+    my_doubly_linked_list.print_list()
+
+    my_doubly_linked_list.reverse()
+
+    print("\nDLL after reverse():")
+    my_doubly_linked_list.print_list()
