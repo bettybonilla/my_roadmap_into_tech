3e1055564b7697ba8ab44c42881ea0b93dc38350
= <=>
2025-05-28 12:02:16 -0400

    Add example file

---
+++ my_learning_path/course_5_udemy_fastapi_the_complete_course_2025/section_5_project_1_fastapi_request_method_logic/marc_code/example_query_parameter.py
@@ -0,0 +1,31 @@
+# Example code for accessing all query arguments from client without needing to define all of them as query parameters
+
+
+from fastapi import FastAPI
+from fastapi import Request
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_item(r: Request) -> dict[str, str]:
+    print(r.query_params)
+    print("--------------")
+    print(r.query_params.keys())
+    print("--------------")
+    print(r.query_params.values())
+    print("--------------")
+    print(r.query_params.items())
+    print("--------------")
+    print(r.query_params.get("shiba"))
+    print("--------------")
+    # just returning random data to ensure the client is hitting the server
+    return {"foo": "bar"}
+
+
+# ----------------------------------------
+
+
+# curl command to interact with the above server code:
+# curl 'localhost:8000/?skip=0&name=marc&hello=world&shiba=hachi'
+# needs to be passed as a string since the & character already exists as a key character and will cause an error
